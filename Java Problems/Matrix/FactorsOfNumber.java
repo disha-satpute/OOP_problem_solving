7de14cc13f3152e6e98005426ebbd3b707b86a3c
Disha Satpute <dishasatpute44@gmail.com>
2024-10-18 22:21:14 +0530

    code added

--- Java Problems/Matrix/FactorsOfNumber.java
+++ Java Problems/Matrix/FactorsOfNumber.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class FactorsOfNumber {
 
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
         Scanner sc = new Scanner(System.in);
 
-        // Ask for user input
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
         System.out.println("Factors of " + num + " are:");
 
-        // Iterate from 1 to the number
         for (int i = 1; i <= num; i++) {
-            // Check if 'i' is a factor of the number
+
             if (num % i == 0) {
                 System.out.println(i); // Print the factor
             }
