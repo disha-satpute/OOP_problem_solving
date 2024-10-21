9d07a5f8840847fa855063205f7dd2da94dc4cb4
Disha Satpute <dishasatpute44@gmail.com>
2024-10-21 23:32:14 +0530

    update program

--- Java Problems/Number System/DuckNo.java
+++ Java Problems/Number System/DuckNo.java
@@ -1,25 +1,22 @@
 /*Find out whether a given number is a Duck number or not.
  A Duck number is a number which has zeroes present in it, but there should be no zero present in the beginning of the number. */
 
-
  import java.util.*;
-
  class DuckNo {
      public static void main(String args[]) {
          Scanner sc = new Scanner(System.in);
 
          System.out.println("Enter a Number:");
-         String num = sc.next();  // Treating the input as a String to check for Duck number
- 
-         // Check if the number is a Duck number
+         String num = sc.next();
+
          boolean isDuck = false;
-         int i = 0;
- 
+      int i = 0;
+
          // Skip leading zeros
          while (i < num.length() && num.charAt(i) == '0') {
              i++;
          }
- 
+
          // Check for a zero after skipping leading zeros
          while (i < num.length()) {
              if (num.charAt(i) == '0') {
