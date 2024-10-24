769ce57120fd7fd35d3e3b8aab448c5855d82eb4
Disha Satpute <dishasatpute44@gmail.com>
2024-10-24 23:02:15 +0530

    update :

--- Java Problems/Number System/FactorsOfNumber.java
+++ Java Problems/Number System/FactorsOfNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FactorsOfNumber {
 
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
@@ -14,7 +14,7 @@ public class FactorsOfNumber {
         for (int i = 1; i <= num; i++) {
 
             if (num % i == 0) {
-                System.out.println(i); // Print the factor
+                System.out.println(i);
             }
         }
     }
