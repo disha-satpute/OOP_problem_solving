3689d47d568042cd2b03427872e4ce0f32efc660
Disha Satpute <dishasatpute44@gmail.com>
2024-10-24 23:02:34 +0530

    update :

--- Java Problems/Number System/GCDOfTwoNumbers.java
+++ Java Problems/Number System/GCDOfTwoNumbers.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class GCDOfTwoNumbers {
 
-    // Method to compute GCD of two numbers using the Euclidean algorithm
     public static int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
@@ -13,20 +12,18 @@ public class GCDOfTwoNumbers {
     }
 
     public static void main(String[] args) {
-        // Create a scanner object to take user input
+
         Scanner sc = new Scanner(System.in);
 
-        // Ask the user to input two numbers
+
         System.out.print("Enter the first number: ");
         int num1 = sc.nextInt();
 
         System.out.print("Enter the second number: ");
         int num2 = sc.nextInt();
 
-        // Find the GCD of the two numbers
         int gcd = gcd(num1, num2);
 
-        // Output the result
         System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
     }
 }
