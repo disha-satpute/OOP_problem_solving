c1f0ed3a1af365bdea138e3b1eff6009699a2050
Disha Satpute <dishasatpute44@gmail.com>
2024-10-25 20:15:36 +0530

    data updated

--- Java Problems/Matrix/MatrixTraceNormal.java
+++ Java Problems/Matrix/MatrixTraceNormal.java
@@ -1,25 +1,23 @@
 //Find the trace and normal of a given matrix. The normal of a matrix is the square root of the sum of squares of all the elements of a matrix. The trace of a matrix is the sum of all the elements present in the principal diagonal (upper left to lower right).
 
 public class MatrixTraceNormal {
-    // Function to calculate the trace and normal of the matrix
-    public static void calculateTraceAndNormal(int[][] matrix) {
-        int N = matrix.length; // Number of rows
-        int M = matrix[0].length; // Number of columns
-        
-        int trace = 0; // Initialize trace
-        double normal = 0.0; // Initialize normal
 
-        // Calculate trace and normal
+    public static void calculateTraceAndNormal(int[][] matrix) {
+        int N = matrix.length;
+        int M = matrix[0].length;
+        
+        int trace = 0;
+        double normal = 0.0;
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (i == j) {
-                    trace += matrix[i][j]; // Add to trace if it's a diagonal element
+                    trace += matrix[i][j];
                 }
-                normal += Math.pow(matrix[i][j], 2); // Add square of element to normal
-            }
+                normal += Math.pow(matrix[i][j], 2); 
         }
 
-        normal = Math.sqrt(normal); // Take square root for normal
+        normal = Math.sqrt(normal); 
 
         // Print results
         System.out.println("Trace of the matrix: " + trace);
