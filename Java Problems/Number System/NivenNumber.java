b9357f9dd6ab3f3d7852f310561e530c4765ed01
Disha Satpute <dishasatpute44@gmail.com>
2024-10-27 15:14:20 +0530

    changes added

--- Java Problems/Number System/NivenNumber.java
+++ Java Problems/Number System/NivenNumber.java
@@ -2,36 +2,31 @@ import java.util.Scanner;
 
 public class NivenNumber {
 
-    // Method to calculate the sum of digits of a number
     public static int sumOfDigits(int num) {
         int sum = 0;
 
-        // Loop to extract and sum each digit of the number
         while (num > 0) {
-            sum += num % 10;  // Add the last digit to the sum
-            num /= 10;        // Remove the last digit from the number
+            sum += num % 10;
+            num /= 10;
         }
 
-        return sum;  // Return the sum of digits
+        return sum;
     }
 
-    // Method to check if the number is a Niven number
-    public static boolean isNiven(int num) {
-        int sum = sumOfDigits(num);  // Get the sum of the digits
 
-        // Check if the number is divisible by the sum of its digits
+    public static boolean isNiven(int num) {
+        int sum = sumOfDigits(num);
+
         return (num % sum == 0);
     }
 
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
+
         Scanner sc = new Scanner(System.in);
 
-        // Ask the user to input a number
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        // Check if the number is a Niven number
         if (isNiven(num)) {
             System.out.println(num + " is a Niven number.");
         } else {
