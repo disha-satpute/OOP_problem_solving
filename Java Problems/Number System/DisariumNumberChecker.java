eec716e7ed7ac610bcd9699c4c80735e7f2f6529
Disha Satpute <dishasatpute44@gmail.com>
2024-10-27 15:12:41 +0530

    changes added

--- Java Problems/Number System/DisariumNumberChecker.java
+++ Java Problems/Number System/DisariumNumberChecker.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 
 public class DisariumNumberChecker {
 
-    // Function to check if a number is a Disarium number
     public static boolean isDisarium(int number) {
         int sum = 0;
         String numStr = Integer.toString(number);
-        // Calculate the sum of digits raised to the power of their positions
+
         for (int i = 0; i < numStr.length(); i++) {
-            int digit = Character.getNumericValue(numStr.charAt(i)); // Get the digit
-            sum += Math.pow(digit, i + 1); // Raise to the power of (i + 1)
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            sum += Math.pow(digit, i + 1);
         }
 
-        // Check if the sum equals the original number
+
         return sum == number;
     }
 
@@ -22,7 +21,6 @@ public class DisariumNumberChecker {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        // Check if the number is a Disarium number
         if (isDisarium(number)) {
             System.out.println(number + " is a Disarium Number.");
         } else {
